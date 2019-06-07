30e226034e5d163372d029f7a7a90107861da380
Ismael Garrido <100346215@alumnos.uc3m.es>
2019-06-06 18:58:17 -0600

    media spark code included

---
+++ media-spark/src/main/java/MediaSpark.java
@@ -0,0 +1,43 @@
+/* MediaSpark.java */
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.*;
+
+
+import java.io.File;
+
+public class MediaSpark {
+  public static void main(String[] args) {
+
+    // Local log file
+    //String logFile = "/Users/isma/Downloads/spark-2.4.0-bin-hadoop2.7/README.md";
+    // Create Spark session
+    SparkSession spark = SparkSession.builder().appName("Media Spark").getOrCreate();
+    //Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+    // A JSON dataset is pointed to by path
+    // Iterate through the directory to input all the timeline JSON files from an event
+    Dataset<Row> timeline = spark.read().json(args[0]);
+
+    // Creates a temporary view using the DataFrame
+    timeline.createOrReplaceTempView("timeline");
+
+    // SQL statements can be run by using the sql methods provided by spark
+    // Query the media block from the tweets of the event
+    Dataset<Row> namesDF = spark.sql("SELECT user.screen_name AS user, extended_tweet.entities.media AS media FROM timeline");
+
+    // Explode the media array block in order to get all the pictures from the same tweet
+    namesDF.select(namesDF.col("user"), explode(namesDF.col("media"))).createOrReplaceTempView("expTimeline");
+
+    // Select the media id, the image link, and the tweet url from the media block
+    Dataset<Row> namesDF2 = spark.sql("SELECT user, col.id AS media_id, col.media_url_https  AS image_link, col.expanded_url AS tweet_url FROM expTimeline");
+
+    // Write the result of the query to our destination JSON file
+    namesDF2.coalesce(1).write().mode(SaveMode.Overwrite).json(args[1]);
+
+    // Stop Spark session
+    spark.stop();
+  }
+}
