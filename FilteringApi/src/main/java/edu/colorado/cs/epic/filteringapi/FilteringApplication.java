52be3a0bfc7b67f4553741b9179e8e5d64191182
Project EPIC <projectepic@Projects-MacBook-Pro.local>
2019-08-08 19:15:08 -0600

    Add CORS to Filtering API

--- FilteringApi/src/main/java/edu/colorado/cs/epic/filteringapi/FilteringApplication.java
+++ FilteringApi/src/main/java/edu/colorado/cs/epic/filteringapi/FilteringApplication.java
@@ -6,11 +6,19 @@ import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
+import edu.colorado.cs.epic.AddAuthToEnv;
 import edu.colorado.cs.epic.filteringapi.resources.FilteringResource;
 import edu.colorado.cs.epic.filteringapi.resources.RootResource;
 
-public class FilteringApplication extends Application<Configuration> {
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.io.IOException;
+import java.util.EnumSet;
+
+public class FilteringApplication extends Application<FilteringConfiguration> {
+  
   public static void main(final String[] args) throws Exception {
     new FilteringApplication().run(args);
   }
@@ -21,7 +29,7 @@ public class FilteringApplication extends Application<Configuration> {
   }
 
   @Override
-  public void initialize(Bootstrap<Configuration> bootstrap) {
+  public void initialize(final Bootstrap<FilteringConfiguration> bootstrap) {
     bootstrap.setConfigurationSourceProvider(
       new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
         new EnvironmentVariableSubstitutor(false)
@@ -30,7 +38,19 @@ public class FilteringApplication extends Application<Configuration> {
   }
 
   @Override
-  public void run(Configuration configuration, Environment environment) {
+  public void run(FilteringConfiguration configuration, Environment environment) throws IOException {
+
+    final FilterRegistration.Dynamic cors =
+        environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+    cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+    AddAuthToEnv.register(environment, configuration.getProduction());
+
+    // Configure CORS parameters
+    cors.setInitParameter("allowedOrigins", "*");
+    cors.setInitParameter("allowedHeaders", "X-Requested-With,Authorization,Content-Type,Accept,Origin");
+    cors.setInitParameter("allowedMethods", "OPTIONS,GET,HEAD");
+
     environment.jersey().register(new RootResource());
     environment.jersey().register(new FilteringResource());
   }
