3cc4659e522460ba3b68e5665bedc794ff5d308d
Ryan Loi <loi.ryan18@gmail.com>
2021-03-15 22:16:00 -0600

    geotag postgres sql creation scripts

---
+++ cloudsql/geotagdb.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS geo_tag_index (
+    id SERIAL PRIMARY KEY,
+    quad_key TEXT,
+    created_at TEXT,
+    username TEXT,
+    user_id_str TEXT,
+    tweet_id_str TEXT,
+    lang TEXT,
+    source TEXT,
+    in_reply_to_user_id_str TEXT,
+    text TEXT,
+    image_link TEXT,
+    is_retweet BOOLEAN,
+    event_id SERIAL REFERENCES events(id),
+    UNIQUE(event_id, tweet_id_str)
+);
